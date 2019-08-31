b61f4640c638cad959a9b6c101e40f3b88180d30
Hwan-seok <tttkhs@khu.ac.kr>
2019-08-31 19:51:24 +0900

    asdf

--- sender.js
+++ sender.js
@@ -2,24 +2,14 @@ const AWS = require("aws-sdk");
 const qURL = require("./qURL");
 AWS.config.loadFromPath("./config.json");
 
-// SQS 객체 생성
-const sqs = new AWS.SQS();
 const PARAMS = {
     QueueUrl: qURL,
     MessageBody: new Date().toString(),
     DelaySeconds: 0,
     MessageGroupId: new Date().toString().replace(/ /gi, "")
 };
-
-// sqs.sendMessage(PARAMS)
-//     .promise()
-//     .then(() => {
-//         console.log("Message 전송 성공");
-//     })
-//     .catch(error => {
-//         console.error(error);
-//     });
-
+// SQS 객체 생성
+const sqs = new AWS.SQS();
 const timeout = 1000;
 setInterval(() => {
     sqs.sendMessage(PARAMS)
